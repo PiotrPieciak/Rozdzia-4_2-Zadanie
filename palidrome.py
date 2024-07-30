7400a47350cf712e55bde40f10db213cbf9f2b82
Piotr Pięciak <piotr.pieciak@gmail.com>
2024-07-30 14:43:22 +0200

    Change function to one line

--- palidrome.py
+++ palidrome.py
@@ -1,10 +1,13 @@
 #funkcja "palindrome" pobiera słowo i sprawdza czy jest palindromem
 def palindrome(word):
-  #finkcja sprawdza czy pobrany argument zapisany w odwrotnej kolejności jest taki sam jak zapisany w pierwotnej kolejności. Jeśli tak zwróci True, jeśli nie zwróci False
-  if word == word[::-1]:
-    return True
-  else:
-    return False
+  #Poniższe linie to dłuższy zapis "return word == word[::-1]"
+  #if word == word[::-1]:
+  #  return True
+  #else:
+  #  return False
+
+  #funkcja sprawdza czy pobrany argument zapisany w odwrotnej kolejności jest taki sam jak zapisany w pierwotnej kolejności. Jeśli tak zwróci True, jeśli nie zwróci False
+  return word == word[::-1]
 
 #w wierszu poniżej wywołujemy naszą funkcję z argumentem "kajak"
 print(palindrome("kajak"))
